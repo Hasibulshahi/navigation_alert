acb78b09e9294e6c9d21ca887d76952956a9a320
Hasibul Hassan <shahi_aust@hotmail.com>
2024-10-27 11:30:17 +0800

    create repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,165 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+void main()
+{
+  runApp(MaterialApp(
+    theme: ThemeData(primaryColor: Colors.blue),
+    debugShowCheckedModeBanner: false,
+    home: const LoginPage(),
+  ));
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFF1E3A8A), // Dark blue
+                Color(0xFF2563EB), // Medium blue
+                Color(0xFF60A5FA), // Light blue
+              ],
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 80,),
+              Image.asset('images/logo_map_white.png'),
+              const SizedBox(height: 15,),
+              const Text(
+                'Navigation Alert',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 30,),
+              Container(
+                height: 500,
+                width: 325,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 20,),
+                    const Text(
+                      'Welcome',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),                      
+                    ),
+                    const SizedBox(height: 10,),
+                    const Text(
+                      'Please Login to Your Account',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey,
+                      ),                      
+                    ),
+                    const SizedBox(height: 30,),
+                    const SizedBox(
+                      width: 250,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Email Address',
+                          suffixIcon: Icon(FontAwesomeIcons.envelope, size: 17),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    const SizedBox(
+                      width: 250,
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          suffixIcon: Icon(FontAwesomeIcons.eyeSlash, size: 17),
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(20, 20, 40, 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'Forget Password',
+                            style: TextStyle(
+                              color: Colors.grey
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 30,),
+                    GestureDetector(
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 250,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          gradient: const LinearGradient(
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                            colors: [
+                              Color(0xFF1E3A8A), // Dark blue
+                              Color(0xFF2563EB), // Medium blue
+                              Color(0xFF60A5FA), // Light blue
+                            ],
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15,),
+                    const Text(
+                      'Or Login using Social Media',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      )
+                    ),
+                    const SizedBox(height: 20,),
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(FontAwesomeIcons.facebookF, color: Colors.blueAccent,),
+                        Icon(FontAwesomeIcons.twitch, color: Colors.blueAccent,),
+                        Icon(FontAwesomeIcons.instagram, color: Colors.blueAccent,),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
